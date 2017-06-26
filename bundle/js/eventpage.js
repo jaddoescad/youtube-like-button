82410badc9110bb16ec7811d502e05597a19dd65
Jad Slim <jadslim@Jads-MacBook-Pro.local>
2017-06-25 22:42:51 -0400

    added log in background

--- bundle/js/eventpage.js
+++ bundle/js/eventpage.js
@@ -1,7 +1,7 @@
 function onCommand(command) {
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    console.log(response);
+    console.log("executed");
   });
 });    
 }
