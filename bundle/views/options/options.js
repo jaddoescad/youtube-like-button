1470ea24574ae91af2f0fa485a31f14c685a0c01
Jad Slim <jadslim@Jads-MacBook-Pro.local>
2017-07-03 18:17:54 -0400

    changed key

--- bundle/views/options/options.js
+++ bundle/views/options/options.js
@@ -1,40 +1,40 @@
-'use strict';
+// 'use strict';
 
-$.fn.serializeObject = function() {
-   var o = {};
-   var a = this.serializeArray();
-   $.each(a, function() {
-       if (o[this.name]) {
-           if (!o[this.name].push) {
-               o[this.name] = [o[this.name]];
-           }
-           o[this.name].push(this.value || '');
-       } else {
-           o[this.name] = this.value || '';
-       }
-   });
-   return o;
-};
+// $.fn.serializeObject = function() {
+//    var o = {};
+//    var a = this.serializeArray();
+//    $.each(a, function() {
+//        if (o[this.name]) {
+//            if (!o[this.name].push) {
+//                o[this.name] = [o[this.name]];
+//            }
+//            o[this.name].push(this.value || '');
+//        } else {
+//            o[this.name] = this.value || '';
+//        }
+//    });
+//    return o;
+// };
 
-function saveOptions() {
-  var options = $('#options').serializeObject();
-  console.log(options);
+// function saveOptions() {
+//   var options = $('#options').serializeObject();
+//   console.log(options);
 
-  chrome.storage.sync.set(options);
-}
+//   chrome.storage.sync.set(options);
+// }
 
-function restoreOptions() {
-  chrome.storage.sync.get({ likeWhat: 'subscribed' }, function(items) {
-    $('input[name="likeWhat"][value="' + items.likeWhat + '"]').click();
-  });
-}
+// function restoreOptions() {
+//   chrome.storage.sync.get({ likeWhat: 'subscribed' }, function(items) {
+//     $('input[name="likeWhat"][value="' + items.likeWhat + '"]').click();
+//   });
+// }
 
-$(document).on('click', 'input[type="radio"]', saveOptions);
+// $(document).on('click', 'input[type="radio"]', saveOptions);
 
-$('#links a').hover(function() {
-  $('#hover-label').html( $(this).find('label').html() );
-}, function() {
-  $('#hover-label').html('');
-});
+// $('#links a').hover(function() {
+//   $('#hover-label').html( $(this).find('label').html() );
+// }, function() {
+//   $('#hover-label').html('');
+// });
 
-restoreOptions();
+// restoreOptions();
